69d4e0a55501c943d384625fc65790faca4fed35
Aymeric Bienfait <Camy.nodream@hotmail.fr>
2024-09-16 13:06:07 +0200

    Initial commit of motaphoto project

---
+++ app/public/wp-content/themes/twentytwentyone-child/footer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+?>
+
+</main><!-- #main -->
+</div><!-- #primary -->
+</div><!-- #content -->
+
+<footer id="colophon" class="site-footer">
+    <?php if ( has_nav_menu( 'footer-menu' ) ) : ?>
+        <nav aria-label="<?php esc_attr_e( 'Footer menu', 'twentytwentyone' ); ?>" class="footer-navigation">
+            <ul class="footer-navigation-wrapper">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'footer-menu',
+                        'items_wrap'     => '%3$s',
+                        'container'      => false,
+                        'depth'          => 1,
+                        'link_before'    => '<span>',
+                        'link_after'     => '</span>',
+                        'fallback_cb'    => false,
+                    )
+                );
+                ?>
+            </ul><!-- .footer-navigation-wrapper -->
+        </nav><!-- .footer-navigation -->
+    <?php else : ?>
+        <p style="color: red;">No footer menu assigned</p>
+    <?php endif; ?>
+</footer><!-- #colophon -->
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
+
---
+++ app/public/wp-content/themes/twentytwentyone-child/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), filemtime(get_stylesheet_directory() . '/style.css'));
+}
+
+function register_my_menus() {
+    register_nav_menus( array(
+        'main-menu'   => __( 'Menu en tete', 'text-domain' ),
+        'footer-menu' => __( 'Menu pied de page', 'text-domain' )
+    ));
+}
+add_action( 'after_setup_theme', 'register_my_menus' );
