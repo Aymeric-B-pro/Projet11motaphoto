052829b43be90d7e84858ca6ae4fff6d2bbf3cb0
Aymeric Bienfait <Camy.nodream@hotmail.fr>
2024-11-14 08:14:08 +0100

    Update

--- app/public/wp-content/themes/twentytwentyone-child/footer.php
+++ app/public/wp-content/themes/twentytwentyone-child/footer.php
@@ -38,24 +38,31 @@
     <?php else : ?>
         <p style="color: red;">No footer menu assigned</p>
     <?php endif; ?>
-        <!-- Lightbox structure -->
-        <div id="lightbox" class="lightbox" style="display: none;">
-            <div class="lightbox-content">
-                <!-- Bouton de fermeture -->
-                <span class="closelightbox">&times;</span>
-                <!-- Navigation pour les photos précédentes et suivantes -->
-                <div class="lightbox-navigation">
-                    <a href="#" class="nav-linklight prev-linklight" id="prev-link">&larr;</a>
-                    <a href="#" class="nav-linklight next-linklight" id="next-link">&rarr;</a>
-                </div>
-                <!-- Image et informations de la lightbox -->
+    <!-- Lightbox structure -->
+    <div id="lightbox" class="lightbox" style="display: none;">
+        <div class="lightbox-content">
+            <!-- Bouton de fermeture -->
+            <span class="closelightbox">&times;</span>
+            
+            <!-- Flexbox contenant les flèches et l'image -->
+            <div class="lightbox-flex-container">
+                <!-- Flèche de navigation précédente -->
+                <a href="#" class="nav-linklight prev-linklight" id="prev-link">&larr;    <p>Précédente</p></a>
+
+                <!-- Image de la lightbox -->
                 <img src="" id="lightbox-image" alt="Lightbox Image">
-                <div class="lightbox-info">
-                    <span class="photo-ref" id="lightbox-ref">Référence non disponible</span>
-                    <span class="photo-category" id="lightbox-category">Catégorie non disponible</span>
-                </div>
+
+                <!-- Flèche de navigation suivante -->
+                <a href="#" class="nav-linklight next-linklight" id="next-link"><p>Suivante</p>    &rarr;</a>
+            </div>
+
+            <!-- Informations de la lightbox -->
+            <div class="lightbox-info">
+                <span class="photo-ref" id="lightbox-ref">Référence non disponible</span>
+                <span class="photo-category" id="lightbox-category">Catégorie non disponible</span>
             </div>
         </div>
+    </div>
 
 <?php wp_footer(); ?>
 <?php get_template_part('template_parts/modal-contact'); ?>
